605e8a30328c4609f8ffcfc6788a0f7f8440c309
Piotr Sokołowski <piotr.sokolowski@orange.com>
2019-07-15 18:53:17 +0200

    cuda :-)

--- src/main/java/vecrecog/CarDetector.java
+++ src/main/java/vecrecog/CarDetector.java
@@ -2,26 +2,29 @@ package vecrecog;
 
 
 import org.bytedeco.javacv.ImageTransformer;
+import org.bytedeco.opencv.opencv_core.GpuMat;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.RectVector;
 import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_cudaobjdetect.CudaCascadeClassifier;
 import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
 import static org.bytedeco.opencv.global.opencv_core.*;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
+
+import org.bytedeco.javacpp.Pointer;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.Objdetect;
 
 public class CarDetector {
 	
-	CascadeClassifier cascade;
+	CudaCascadeClassifier cascade;
 		
 	int absoluteFaceSize=0;
 	int absoluteMaxFaceSize=0;
 	
-	public CarDetector() {
-		cascade = new CascadeClassifier();		
-		cascade.load("/tmp/resources/cars.xml");		
+	public CarDetector() {					
+		cascade=CudaCascadeClassifier.create("/tmp/resources/cars.xml");		
 	}
 	
 	public RectVector detect(Mat frame)
@@ -53,9 +56,12 @@ public class CarDetector {
 			}
 		}
 		
-		try {
-			cascade.detectMultiScale(grayFrame, results, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
-					new Size(absoluteFaceSize, absoluteFaceSize), new Size(absoluteMaxFaceSize,absoluteMaxFaceSize));
+		try {			
+			/*cascade.detectMultiScale(grayFrame, results, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
+					new Size(absoluteFaceSize, absoluteFaceSize), new Size(absoluteMaxFaceSize,absoluteMaxFaceSize));*/
+			Mat gpuObjects=new Mat();
+			cascade.detectMultiScale(grayFrame,gpuObjects); 
+			cascade.convert(gpuObjects, results);
 			System.out.println("DETECTION! "+results.size());
 		} catch (Exception e) {
 			System.out.println("B³¹d "+e);
